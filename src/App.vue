d6871b87b9b36433f5b3585d9f483491fb16d410
Aaron Wilson <aaron@unionstudios.com>
2022-05-19 09:54:47 -0400

    creating new component files

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+import AppHeader from '@/components/AppHeader.vue'
 </script>
 
 <template>
@@ -8,8 +8,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-Aaron Wilson
+      <AppHeader msg="Product Design, UXUI, Frontend Development" />
       <!-- <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,12 +10,10 @@ defineProps({
 <template>
   <div class="greetings">
     AARON WILSON
-    <!-- <h1 class="green">{{ msg }}</h1>
+    <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
-    </h3> -->
+      25+ years designing and building exciting and intuitive user centric web solutions.
+    </h3>
   </div>
 </template>
 
